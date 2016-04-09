a4e5d0477d72740494cfb35e4887e28afc9d17a9
Ishwar Agarwal <a.isshwar@gmail.com>
2016-04-09 18:31:52 +0000

    added the view for the admin panel

--- app/__init__.py
+++ app/__init__.py
@@ -12,12 +12,6 @@ from app import allImports
 # Include an import for every python file that is serving a webpage
 #import your new python files here. It is not a part of the module until it is imported
 print("Starting application") #I don't see this print - Cody Myers
-from app import createPrograms
-from app import readPrograms
-from app import editPrograms
-from app import deletePrograms
-from app import RelationalExample
-from app import readCourses
-from app import start
+
 from app import courseLanding
 from app import course
--- app/course.py
+++ app/course.py
@@ -7,6 +7,8 @@ def courses(tID, prefix):
       divisions = Division.select()
       programs = Program.select()
       subjects = Subject.select()
+      
+      subject = Subject.get(Subject.prefix == prefix)
       courses = Course.select().where(Course.prefix == prefix).where(Course.term == tID)
       
       instructors = {}
@@ -14,16 +16,35 @@ def courses(tID, prefix):
         instructors[course.cId] = InstructorCourse.select().where(InstructorCourse.course == course.cId)
       
       username = authUser(request.environ)
+      print username
       # admin = User.select(User.isAdmin).where(User.username == username)
       admin = User.get(User.username == username)
-      divisionChair = DivisionChair.select().where(DivisionChair.username == username)
+      divisionChair = DivisionChair.select().where(DivisionChair.username == username).where(DivisionChair.did == subject.pid.division.dID)
       programChair = ProgramChair.select().where(ProgramChair.username == username)
       if admin.isAdmin:
         print "isAdmin: True"
       elif divisionChair.exists():
-        print "isDivisionChair: true"
+        print "we are here division chair"
+        return render_template("programAdmin.html",
+                              cfg      = cfg,
+                              courses = courses,
+                              instructors = instructors,
+                              programs = programs,
+                              divisions = divisions,
+                              subjects = subjects,
+                              term = tID
+                            )
       elif programChair.exists():
-        print 'isProgramChair: true'
+        print("we are here")
+        return render_template("programAdmin.html",
+                              cfg      = cfg,
+                              courses = courses,
+                              instructors = instructors,
+                              programs = programs,
+                              divisions = divisions,
+                              subjects = subjects,
+                              term = tID
+                            )
       else:
         print 'false'
         
