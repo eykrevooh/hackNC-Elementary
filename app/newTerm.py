0245be1ad0852d38499203edf4fbe6cc3c638873
Cody Myers <codymyers63@gmail.com>
2016-06-06 20:19:03 +0000

    FIXED: (newTerm.py) We needed to force the query in order for it to go into the database.

--- app/newTerm.py
+++ app/newTerm.py
@@ -4,10 +4,13 @@ def newterm():
   page = request.path  
   username = authUser(request.environ)
   admin = User.get(User.username == username)
-  if admin.isAdmin:
-    data = request.form
-    term = Term(termCode = int(data['termCode']), name = data['termName'], editable = True)
-    term.save()
+  try: 
+    if admin.isAdmin:
+      data = request.form
+      newterm = Term(termCode = int(data['termCode']), name = data['termName'], editable = True)
+      newterm.save(force_insert=True)
+  except Exception as e:
+    log.writer("ERROR","newTerm",e)
   message = "Term: Term {} has been created".format(data['termCode'])
   log.writer("INFO", page, message)
   flash("Term successfully created")
