4a3c830c25999ad33e1fb2a3e88b71a3f4d9cff7
Cody Myers <codymyers63@gmail.com>
2016-06-07 20:29:31 +0000

    WORKING ON: (editTerm.py) Clicking the icons will no longer crash the system, however issue #6 still needs to be completed

--- app/editTerm.py
+++ app/editTerm.py
@@ -4,7 +4,7 @@ def editterm():
     username = authUser(request.environ)
     admin = User.get(User.username == username)
     if admin.isAdmin:
-      page = request.page
+      page =  "/" + request.url.split("/")[-1]
       data = request.form
       term = Term.get(Term.termCode == data['termCode'])
       term.editable = not term.editable
@@ -12,3 +12,4 @@ def editterm():
         
       message = "Term: term {} has been editable".format(data['termCode'])
       log.writer("INFO", page, message)
+      return redirect(url_for("systemManagement"))
