1d77c210c696df90617e37b819d7e8d44c074fd2
Cody Myers <codymyers63@gmail.com>
2016-06-07 19:08:01 +0000

    FIXED: (config.yanl) The system is now looking at the correct database location

--- app/programManagement.py
+++ app/programManagement.py
@@ -4,23 +4,23 @@ from updateCourse import DataUpdate
 
 @app.route("/admin/programManagement/<pid>", methods=["GET", "POST"])
 def adminProgramManagement(pid):
-  if (request.method == "GET"):
-      username = authUser(request.environ)
-      admin = User.get(User.username == username)
-      if admin.isAdmin:
-         users = User.select()
-         divisions = Division.select()
-         programs  = Program.select()
-         program = Program.get(Program.pID == pid)
-         programChairs = {}
-         programChairs[program.pID] = ProgramChair.select().where(ProgramChair.pid == program.pID)
-         
-         return render_template("editProgram.html",
-                                 program       = program,
-                                 programChairs = programChairs,
-                                 cfg           = cfg,
-                                 users         = users,
-                                 divisions     = divisions,
-                                 programs      = programs,
-                                 isAdmin       = admin.isAdmin)
+  # if (request.method == "GET"):
+   username = authUser(request.environ)
+   admin = User.get(User.username == username)
+   if admin.isAdmin:
+      users = User.select()
+      divisions = Division.select()
+      programs  = Program.select()
+      program = Program.get(Program.pID == pid)
+      programChairs = {}
+      programChairs[program.pID] = ProgramChair.select().where(ProgramChair.pid == program.pID)
       
+      return render_template("editProgram.html",
+                              program       = program,
+                              programChairs = programChairs,
+                              cfg           = cfg,
+                              users         = users,
+                              divisions     = divisions,
+                              programs      = programs,
+                              isAdmin       = admin.isAdmin)
+   
