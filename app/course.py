efec14657e3b1fcc65421582d3f35ebd704a35e3
Jesson Soto <jessons13@gmail.com>
2016-06-10 12:24:12 +0000

    FIXED: First bug in [Issue # 18]

--- app/course.py
+++ app/course.py
@@ -2,41 +2,38 @@ from allImports import *
 from updateCourse import DataUpdate
 
 
-
 @app.route("/courses/<tID>/<prefix>", methods=["GET", "POST"])
 def courses(tID, prefix):
   username = authUser(request.environ)
+  
+  # These are the necessary components of the sidebar. Should we move them somewhere else?
+  divisions = Division.select()
+  programs = Program.select()
+  subjects = Subject.select()
+  
+  # Checking the permissions of the user.
+  # we need the subject to know if someone if a division chair or a program chair
+  subject = Subject.get(Subject.prefix == prefix)
+  users   = User.select(User.username, User.firstName, User.lastName)
+  
+  # Checking if person is division chair or program chair
   admin = User.get(User.username == username)
+  divisionChair = DivisionChair.select().where(DivisionChair.username == username).where(DivisionChair.did == subject.pid.division.dID)
+  programChair  = ProgramChair.select().where(ProgramChair.username == username).where(ProgramChair.pid == subject.pid.pID)
+  
+  terms = Term.select()
   print admin
   if (request.method == "GET"):
       
       print username
       
-      
-      # These are the necessary components of the sidebar. Should we move them somewhere else?
-      divisions = Division.select()
-      programs = Program.select()
-      subjects = Subject.select()
-      
-      terms = Term.select()
-      
-      
-      # we need the subject to know if someone if a division chair or a program chair
-      subject = Subject.get(Subject.prefix == prefix)
-      users   = User.select(User.username, User.firstName, User.lastName)
-       # We need these for populating add course
+      # We need these for populating add course
       courseInfo = BannerCourses.select().where(BannerCourses.subject == prefix).order_by(BannerCourses.number)
       
       schedules = BannerSchedule.select()
       
-      # Checking if person is division chair or program chair
-      divisionChair = DivisionChair.select().where(DivisionChair.username == username).where(DivisionChair.did == subject.pid.division.dID)
-      programChair  = ProgramChair.select().where(ProgramChair.username == username).where(ProgramChair.pid == subject.pid.pID)
-      
-      
       courses = Course.select().where(Course.prefix == prefix).where(Course.term == tID)
      
-      
       instructors = {}
       for course in courses:
         instructors[course.cId] = InstructorCourse.select().where(InstructorCourse.course == course.cId)
@@ -57,18 +54,19 @@ def courses(tID, prefix):
                               isAdmin  = admin.isAdmin
                             )
 
-      return render_template("program.html",
-                              cfg      = cfg,
-                              courses = courses,
-                              instructors = instructors,
-                              programs = programs,
-                              divisions = divisions,
-                              subjects = subjects,
-                              currentTerm = tID,
-                              allTerms = terms
-                            )
+      else:
+        return render_template("program.html",
+                                cfg      = cfg,
+                                courses = courses,
+                                instructors = instructors,
+                                programs = programs,
+                                divisions = divisions,
+                                subjects = subjects,
+                                currentTerm = tID,
+                                allTerms = terms
+                              )
   if (request.method == "POST"):
-    if admin.isAdmin:
+    if admin.isAdmin or divisionChair.exists() or programChair.exists():
       page = request.path
       data   = request.form
       print data
