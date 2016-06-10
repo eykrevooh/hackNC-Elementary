2a92d9a74272c296e961586d87994522d58b5aa8
Cody Myers <codymyers63@gmail.com>
2016-06-10 15:37:47 +0000

    FIXED: (changeAdmin.py,systemManagement.html) Fixed issue #23

--- app/changeAdmin.py
+++ app/changeAdmin.py
@@ -6,17 +6,18 @@ def changeAdmin():
   admin = User.get(User.username == username)
   if admin.isAdmin:
     data = request.form
-    print data
+    print data['admin']
     user = User.get(User.username == data['admin'])
     user.isAdmin = not user.isAdmin
     user.save()
-    
     #log changes
     message = "User: {0} is now {1} an admin".format(user.username, (lambda:"not", lambda:"")[user.isAdmin]())
     log.writer("INFO", page, message)
-    
     flash("Administrator successfully added")
-    return redirect(url_for('systemManagement'))
+    if username == data['admin']: #IF THE USER DELETES THEMSELVES SEND THEM BACK TO THE HOME PAGE
+      return redirect('/')
+    else:
+      return redirect(url_for('systemManagement'))
   else:
     log.writer("ERROR", page, log.lowPrivilege)
     return render_template("404.html")
