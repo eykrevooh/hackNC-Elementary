df0ca620ac3b057294d32b5e0567856703b7fdc9
Cody Myers <codymyers63@gmail.com>
2016-06-14 19:22:19 +0000

    WORKING ON:[ISSUE #35] Fixed the .py and the .html to show the new elements in the form. Now I need to handle the post with the new id's in the form

--- app/systemManagement.py
+++ app/systemManagement.py
@@ -9,30 +9,28 @@ def systemManagement():
       username = authUser(request.environ)
       admin = User.get(User.username == username)
       if admin.isAdmin:
-         terms       = Term.select()
-         semesters   = Term.select(Term.semester).distinct()
-         users       = User.select()
-         program     = Program.get()
-         today       = datetime.date.today()
+         terms          = Term.select()
+         termSemesters  = Term.select(Term.semester).distinct()
+         users          = User.select()
+         program        = Program.get()
+         today          = datetime.date.today()
          
          #WE WANT THE USER TO HAVE THE ABILITY TO SELECT A YEAR AGO AND THREE YEARS PAST THE CURRENT YEAR
          years       = []
-         year  = int(time.strftime("%Y")) - 1 #START WITH ONE YEAR AGO
+         year  = int(time.strftime("%Y")) - 1   #START WITH ONE YEAR AGO
          for x in range(5):
             if x == 0:
-               years.append(str(year)) #APPEND THE VALUE OF A YEAR PLUS ONE
+               years.append(str(year))          #APPEND THE ONE YEAR AGO
             else:
                year += 1
-               years.append(str(year)) #APPEND  
-         print years
+               years.append(str(year))          #APPEND CURRENT NUMBER PLUS ONE
          
          return render_template("systemManagement.html",
                                  terms          = terms,
-                                 semesters      = semesters,
-                              
+                                 termSemesters  = termSemesters,
+                                 years          = years,
                                  program        = program,
                                  cfg            = cfg,
                                  users          = users,
-                                
                                  isAdmin        = admin.isAdmin,
                                  today          = today)
