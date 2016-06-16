6346c0e03f4d21df314bd50d300ddb0910bde811
Cody Myers <codymyers63@gmail.com>
2016-06-16 16:15:24 +0000

    WORKING ON: [ISSUE #34] The arrows are in and functional, but I need to fix the spacing of them inside of the navbar

--- app/static/js/local/sidebar.js
+++ app/static/js/local/sidebar.js
@@ -2,5 +2,9 @@ $(document).ready(function () {
 	$('label.tree-toggler').click(function () {
 		$(this).parent().children('ul.tree').toggle(300);
 	});
+	
+	$('span.glyphicon').click(function(){
+		$(this).toggleClass('glyphicon-menu-down')
+	})
 });
 
