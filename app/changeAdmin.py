8013b84f673c2fd72a606b01da4c98e68197be4f
Cody Myers <codymyers63@gmail.com>
2016-06-17 18:23:26 +0000

    FIXED: [ISSUE #36] The issue if fixed with an if statement

--- app/changeAdmin.py
+++ app/changeAdmin.py
@@ -6,20 +6,25 @@ def changeAdmin():
   admin = User.get(User.username == username)
   if admin.isAdmin:
     data = request.form
-    print data['admin']
-    user = User.get(User.username == data['admin'])
-    user.isAdmin = not user.isAdmin
-    user.save()
-    #log changes
-    message = "User: {0} is now {1} an admin".format(user.username, (lambda:"not", lambda:"")[user.isAdmin]())
-    log.writer("INFO", page, message)
-    if "not" in message:
-      flash("Administrator {0} {1} successfully removed".format(user.firstName, user.lastName))
+    if data['admin'] != "None":
+      user = User.get(User.username == data['admin'])
+      user.isAdmin = not user.isAdmin
+      user.save()
+      #log changes
+      message = "User: {0} is now {1} an admin".format(user.username, (lambda:"not", lambda:"")[user.isAdmin]())
+      log.writer("INFO", page, message)
+      if "not" in message:
+        flash("Administrator {0} {1} successfully removed".format(user.firstName, user.lastName))
+      else:
+        flash("Administrator {0} {1} successfully added".format(user.firstName, user.lastName))
+      if username == data['admin']: #IF THE USER DELETES THEMSELVES SEND THEM BACK TO THE HOME PAGE
+        return redirect('/')
+      else:
+        return redirect(url_for('systemManagement'))
     else:
-      flash("Administrator {0} {1} successfully added".format(user.firstName, user.lastName))
-    if username == data['admin']: #IF THE USER DELETES THEMSELVES SEND THEM BACK TO THE HOME PAGE
-      return redirect('/')
-    else:
+      message = "A username was not selected during the process."
+      log.writer("INFO", page, message)
+      flash(message)
       return redirect(url_for('systemManagement'))
   else:
     log.writer("ERROR", page, log.lowPrivilege)
