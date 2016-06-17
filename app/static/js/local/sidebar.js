27f300e9c48a7715342e1b9b2518eb3d7f6db702
Cody Myers <codymyers63@gmail.com>
2016-06-17 18:05:50 +0000

    FIXED: [ISSUE #30] The button is disabled and changes color

--- app/static/js/local/sidebar.js
+++ app/static/js/local/sidebar.js
@@ -1,13 +1,8 @@
 function toggleArrowIcon(element,ID) {
-	console.log("Inside of the toggleArrowIcon function:")
-	console.log(element)
-	console.log(ID)
 	$(element).toggleClass('glyphicon-menu-down');
 	toggleID(ID);
 }
 
 function toggleID(ID) {
-	console.log("Inside of the toggleID function:")
-	console.log(ID)
 	$(ID).parent().children('ul.tree').toggle(300);
 }
