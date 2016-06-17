13c2ee0be88c59b144f4084a186021b5b51fc200
Cody Myers <codymyers63@gmail.com>
2016-06-17 14:17:32 +0000

    FIXED: [ISSUE #34] Problem with matching ID'S in the javascript

--- app/static/js/local/sidebar.js
+++ app/static/js/local/sidebar.js
@@ -1,10 +1,13 @@
-$(document).ready(function () {
-	$('label.tree-toggler').click(function () {
-		$(this).parent().children('ul.tree').toggle(300);
-	});
-	
-	$('span.glyphicon').click(function(){
-		$(this).toggleClass('glyphicon-menu-down')
-	})
-});
+function toggleArrowIcon(element,ID) {
+	console.log("Inside of the toggleArrowIcon function:")
+	console.log(element)
+	console.log(ID)
+	$(element).toggleClass('glyphicon-menu-down');
+	toggleID(ID);
+}
 
+function toggleID(ID) {
+	console.log("Inside of the toggleID function:")
+	console.log(ID)
+	$(ID).parent().children('ul.tree').toggle(300);
+}
