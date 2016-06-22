e8806dea8dbd0fa6b9681927f7aa06169ade3e71
Cody Myers <codymyers63@gmail.com>
2016-06-22 19:52:24 +0000

    WORKING ON: The new course management system. I have the navbar now complete

--- app/courseManagement.py
+++ app/courseManagement.py
@@ -2,16 +2,58 @@
 from allImports import *
 from updateCourse import DataUpdate
 
-#GRAB THE URL HERE
-@app.route("/courseManagement", methods=["GET", "POST"])
-def courseManagement():
-    ##DATA NEEDED FOR SIDEBAR##
+@app.route("/courseManagement/crossListed/", defaults={'sid':0}, methods=["GET", "POST"])
+@app.route("/courseManagement/crossListed/<sid>", methods = ["GET"])
+def crossListed(sid):
+    if sid == 0:
+        sid = Term.select(Term.termCode).get()
+    # do stuff
+     ##DATA NEEDED FOR SIDEBAR##
+    page = "crossListed"
     terms = Term.select().order_by(-Term.termCode)
     ##DATA NEEDED FOR SIDEBAR##
     username = authUser(request.environ)
     admin = User.get(User.username == username)
-    return render_template("courseManagement.html",
+    return render_template("crossListed.html",
                             cfg              = cfg,
                             isAdmin          = admin.isAdmin,
-                            allTerms      = terms
+                            allTerms      = terms,
+                            page = page,
+                            currentTerm     = sid
                           )
+
+@app.route("/courseManagement/conflicts/<sid>", methods = ["GET"])
+def conflictsListed(sid):
+    # do stuff
+     ##DATA NEEDED FOR SIDEBAR##
+    page = "conflicts"
+    terms = Term.select().order_by(-Term.termCode)
+    ##DATA NEEDED FOR SIDEBAR##
+    username = authUser(request.environ)
+    admin = User.get(User.username == username)
+    return render_template("conflicts.html",
+                            cfg              = cfg,
+                            isAdmin          = admin.isAdmin,
+                            allTerms      = terms,
+                            page = page,
+                            currentTerm     = sid
+                          )
+
+
+@app.route("/courseManagement/tracker/<sid>", methods = ["GET"])
+def trackerListed(sid):
+    # do stuff
+     ##DATA NEEDED FOR SIDEBAR##
+    page = "tracker"
+    terms = Term.select().order_by(-Term.termCode)
+    ##DATA NEEDED FOR SIDEBAR##
+    username = authUser(request.environ)
+    admin = User.get(User.username == username)
+    return render_template("tracker.html",
+                            cfg              = cfg,
+                            isAdmin          = admin.isAdmin,
+                            allTerms      = terms,
+                            page = page,
+                            currentTerm     = sid
+                          )
+
--- app/logic/courseManagement.py
+++
@@ -1,6 +0,0 @@
-#ADD ANY IMPORTS H
-def crossListed():
-    username = authUser(request.environ)
-    admin = User.get(User.username == username)
-    crossListedCourses = Course.select().where(Course.crossListed == 1).order_by(+Course.schedule).order_by(+Course.rid)
-  return crossListed()
