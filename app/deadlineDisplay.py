861edd2b1a796d489719ca355cc842958863f8da
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-07-13 19:55:33 +0000

    Document and Add Authorized class

--- app/deadlineDisplay.py
+++ app/deadlineDisplay.py
@@ -1,19 +1,23 @@
 from allImports import *
 from updateCourse import DataUpdate
+from getAuthUser import AuthorizedUser
 import datetime
 
 
 @app.route("/", methods=["GET", "POST"])
 def deadlineDisplay():
    if (request.method == "GET"):
-      username = authUser(request.environ)
-      admin = User.get(User.username == username)
+      authorizedUser = AuthorizedUser()
+      
+      # we don't want show repeated dates
       dates = Deadline.select(Deadline.date).distinct().order_by(Deadline.date)
+      
       deadlines = []
       for date in dates:
+         # assign the deadline object to a date for easy access
          deadlines.append((date.date, Deadline.select().where(Deadline.date == date.date)))
-      print deadlines
       
+      # we don't want to show deadlines past today
       today = datetime.date.today()
          
    return render_template("deadline.html",
