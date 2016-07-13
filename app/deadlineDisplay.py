65549689a3f17ffd5bd9a42d3da51d0e8a01b63f
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-07-13 20:28:07 +0000

    run autopep8 on these files

--- app/deadlineDisplay.py
+++ app/deadlineDisplay.py
@@ -6,22 +6,26 @@ import datetime
 
 @app.route("/", methods=["GET", "POST"])
 def deadlineDisplay():
-   if (request.method == "GET"):
-      authorizedUser = AuthorizedUser()
-      
-      # we don't want show repeated dates
-      dates = Deadline.select(Deadline.date).distinct().order_by(Deadline.date)
-      
-      deadlines = []
-      for date in dates:
-         # assign the deadline object to a date for easy access
-         deadlines.append((date.date, Deadline.select().where(Deadline.date == date.date)))
-      
-      # we don't want to show deadlines past today
-      today = datetime.date.today()
-         
-   return render_template("deadline.html",
-                           cfg = cfg,
-                           isAdmin = admin.isAdmin,
-                           deadlines = deadlines,
-                           today = today)
+    if (request.method == "GET"):
+        authorizedUser = AuthorizedUser()
+
+        # we don't want show repeated dates
+        dates = Deadline.select(
+            Deadline.date).distinct().order_by(
+            Deadline.date)
+
+        deadlines = []
+        for date in dates:
+            # assign the deadline object to a date for easy access
+            deadlines.append(
+                (date.date, Deadline.select().where(
+                    Deadline.date == date.date)))
+
+        # we don't want to show deadlines past today
+        today = datetime.date.today()
+
+    return render_template("deadline.html",
+                           cfg=cfg,
+                           isAdmin=admin.isAdmin,
+                           deadlines=deadlines,
+                           today=today)
--- app/deadlineManagement.py
+++ app/deadlineManagement.py
@@ -4,50 +4,55 @@ from app.logic.getAuthUser import AuthorizedUser
 import datetime
 
 
-@app.route("/deadline/create", methods=[ "POST"])
+@app.route("/deadline/create", methods=["POST"])
 def deadlineCreate():
-   page = "/" + request.url.split("/")[-1]
-   authorizedUser = AuthorizedUser()
-   if authorizedUser.isAdmin():
+    page = "/" + request.url.split("/")[-1]
+    authorizedUser = AuthorizedUser()
+    if authorizedUser.isAdmin():
         data = request.form
-        
-        deadline = Deadline.create(term = data['term'], description=data['deadlineDescription'], date=data['deadlineDate'])
+
+        deadline = Deadline.create(
+            term=data['term'],
+            description=data['deadlineDescription'],
+            date=data['deadlineDate'])
         deadline.save()
-        
+
         message = "Deadline: {0} has been added".format(deadline.description)
         log.writer("INFO", page, message)
         flash("Your Deadline has been created")
-   return redirect(url_for("systemManagement"))
-   
-   
-@app.route("/deadline/edit", methods=[ "POST"])
+    return redirect(url_for("systemManagement"))
+
+
+@app.route("/deadline/edit", methods=["POST"])
 def deadlineEdit():
-   page = "/" + request.url.split("/")[-1]
-   authorizedUser = AuthorizedUser
-   if authorizedUser.isAdmin():
+    page = "/" + request.url.split("/")[-1]
+    authorizedUser = AuthorizedUser
+    if authorizedUser.isAdmin():
         data = request.form
-        
+
         deadline = Deadline.get(Deadline.id == data['id'])
         deadline.description = data['deadlineDescription']
         deadline.save()
-        
-        message = "Deadline: has been edited to {0}".format(deadline.description)
+
+        message = "Deadline: has been edited to {0}".format(
+            deadline.description)
         log.writer("INFO", page, message)
         flash("Your Deadline has been edited")
-   return redirect(url_for("deadlineDisplay"))
-   
+    return redirect(url_for("deadlineDisplay"))
+
+
 @app.route("/deleteDeadline", methods=["POST"])
 def deleteDeadline():
-   page = r"/" + request.url.split("/")[-1]
-   username = authUser(request.environ)
-   admin = User.get(User.username == username)
-   if admin.isAdmin:
-      data = request.form
-      
-      deadline = Deadline.get(Deadline.id == int(data['id']))
-      deadline.delete_instance()
-      
-      message = "Deadline: {0} has been deleted".format(deadline.description)
-      log.writer("INFO", page, message)
-      flash("Your Deadline has been deleted")
-   return redirect(url_for("deadlineDisplay"))
+    page = r"/" + request.url.split("/")[-1]
+    username = authUser(request.environ)
+    admin = User.get(User.username == username)
+    if admin.isAdmin:
+        data = request.form
+
+        deadline = Deadline.get(Deadline.id == int(data['id']))
+        deadline.delete_instance()
+
+        message = "Deadline: {0} has been deleted".format(deadline.description)
+        log.writer("INFO", page, message)
+        flash("Your Deadline has been deleted")
+    return redirect(url_for("deadlineDisplay"))
