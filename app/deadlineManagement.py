679c787f7f7f29a20e38b186b509acfe19e6f8a5
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-07-13 20:26:02 +0000

    Add authorizedUser class

--- app/deadlineManagement.py
+++ app/deadlineManagement.py
@@ -1,17 +1,19 @@
 from allImports import *
 from updateCourse import DataUpdate
+from app.logic.getAuthUser import AuthorizedUser
 import datetime
 
 
 @app.route("/deadline/create", methods=[ "POST"])
 def deadlineCreate():
    page = "/" + request.url.split("/")[-1]
-   username = authUser(request.environ)
-   admin = User.get(User.username == username)
-   if admin.isAdmin:
+   authorizedUser = AuthorizedUser()
+   if authorizedUser.isAdmin():
         data = request.form
-        deadline = Deadline(term = data['term'], description=data['deadlineDescription'], date=data['deadlineDate'])
+        
+        deadline = Deadline.create(term = data['term'], description=data['deadlineDescription'], date=data['deadlineDate'])
         deadline.save()
+        
         message = "Deadline: {0} has been added".format(deadline.description)
         log.writer("INFO", page, message)
         flash("Your Deadline has been created")
@@ -21,13 +23,14 @@ def deadlineCreate():
 @app.route("/deadline/edit", methods=[ "POST"])
 def deadlineEdit():
    page = "/" + request.url.split("/")[-1]
-   username = authUser(request.environ)
-   admin = User.get(User.username == username)
-   if admin.isAdmin:
+   authorizedUser = AuthorizedUser
+   if authorizedUser.isAdmin():
         data = request.form
+        
         deadline = Deadline.get(Deadline.id == data['id'])
         deadline.description = data['deadlineDescription']
         deadline.save()
+        
         message = "Deadline: has been edited to {0}".format(deadline.description)
         log.writer("INFO", page, message)
         flash("Your Deadline has been edited")
@@ -40,8 +43,10 @@ def deleteDeadline():
    admin = User.get(User.username == username)
    if admin.isAdmin:
       data = request.form
+      
       deadline = Deadline.get(Deadline.id == int(data['id']))
       deadline.delete_instance()
+      
       message = "Deadline: {0} has been deleted".format(deadline.description)
       log.writer("INFO", page, message)
       flash("Your Deadline has been deleted")
