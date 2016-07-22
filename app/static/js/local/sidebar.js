1b518c79d24f6ce22dd21ff93c53d59b494ad5a0
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-07-22 13:27:46 +0000

    style js

--- app/static/js/local/sidebar.js
+++ app/static/js/local/sidebar.js
@@ -1,45 +1,56 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
   var trigger = $('.hamburger'),
-      overlay = $('.overlay'),
-     isClosed = false;
+    overlay = $('.overlay'),
+    isClosed = false;
 
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-// position the hamburger and show. Needed so that the hamburger is positioned correctly
-$(".hamburger").offset({ top: 70, left: 30});
-$(".hamburger").show()
-    function hamburger_cross() {
+  trigger.click(function() {
+    hamburger_cross();
+  });
+  // position the hamburger and show. 
+  //Needed so that the hamburger 
+  //is positioned correctly
+  $(".hamburger").offset({
+    top: 70,
+    left: 30
+  });
+  $(".hamburger").show()
 
-      if (isClosed == true) {          
-        overlay.hide();
-        trigger.removeClass('is-open');
-        trigger.addClass('is-closed');
-        $(".hamburger").animate({ top: 70, left: 30});
-        isClosed = false;
-      } else {   
-        overlay.show();
-        trigger.removeClass('is-closed');
-        trigger.addClass('is-open');
-        $(".hamburger").animate({ top: 70, left: 230});
-        isClosed = true;
-      }
+  function hamburger_cross() {
+
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      $(".hamburger").animate({
+        top: 70,
+        left: 30
+      });
+      isClosed = false;
+    }
+    else {
+      overlay.show();
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      $(".hamburger").animate({
+        top: 70,
+        left: 230
+      });
+      isClosed = true;
+    }
   }
-  
-  $('[data-toggle="offcanvas"]').click(function () {
-        $('#wrapper').toggleClass('toggled');
-  });  
+
+  $('[data-toggle="offcanvas"]').click(function() {
+    $('#wrapper').toggleClass('toggled');
+  });
 });
 
-function toggleArrowIcon(iconID,labelID) {
-	console.log(iconID);
-	console.log(labelID);
-	$(iconID).toggleClass('glyphicon-menu-down');
-	toggleLabel(labelID);
+function toggleArrowIcon(iconID, labelID) {
+  console.log(iconID);
+  console.log(labelID);
+  $(iconID).toggleClass('glyphicon-menu-down');
+  toggleLabel(labelID);
 }
 
 function toggleLabel(labelID) {
-	$(labelID).parent().children('ul.tree').toggle(300);
+  $(labelID).parent().children('ul.tree').toggle(300);
 }
-
