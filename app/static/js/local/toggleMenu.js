3a0d668e9145f238c5580003f65878d99a8b4473
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-07-22 13:23:51 +0000

    add sidebar to division management

--- app/static/js/local/toggleMenu.js
+++
@@ -1,5 +0,0 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-    this.innerHTML = (this.innerHTML == 'Close Sidebar') ? 'Open Sidebar' : 'Close Sidebar' ;
-});
