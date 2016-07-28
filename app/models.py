a4dde3fbaffd8de31e92912dbb913e8ecc6c6617
Cody Myers <myersco@cs-cas.berea.edu>
2016-07-28 14:34:36 -0400

    Made the filepath for db dynamic

--- app/models.py
+++ app/models.py
@@ -5,9 +5,9 @@ import os
 from app.loadConfig import *
 here = os.path.dirname(__file__)
 cfg       = load_config(os.path.join(here, 'config.yaml'))
-
+db	  = os.path.join(here,'../',cfg['databases']['dev']) 
 # mainDB    = SqliteDatabase(cfg['databases']['dev'])
-mainDB    = SqliteDatabase(cfg['databases']['dev'],
+mainDB    = SqliteDatabase(db,
                           pragmas = ( ('busy_timeout',  100),
                                       ('journal_mode', 'WAL')
                                   ),
