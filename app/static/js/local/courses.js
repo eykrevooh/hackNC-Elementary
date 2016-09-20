74cc47c064bcffe445b3c9b536ce24c26b154368
Cody Myers <codymyers63@gmail.com>
2016-09-20 19:03:38 +0000

    Fixed the frontend issues with Special Topics

--- app/static/js/local/courses.js
+++ app/static/js/local/courses.js
@@ -1,42 +1,36 @@
+console.log("course js loaded")
 
 function getSelectedCourse(elementId) {
    var selectedCourse = document.getElementById(elementId);
    
-   if (selectedCourse.selectedIndex == -1)
+   if (selectedCourse.selectedIndex == -1){
       return null;
-   
+   }
    return selectedCourse.options[selectedCourse.selectedIndex].text;
 }
 
-function specialTopicsName(){
+function stn(){
+   console.log('here');
    var courseTitle = getSelectedCourse('courseInfo');
    if (courseTitle === "---"){
       document.getElementById("submitAdd").disabled = true;
    }
    else{
       document.getElementById("submitAdd").disabled = false;
-   }
-   $(".specialTopics").remove();
-   if (courseTitle != "---"){
-      //The code below removes everything that isn't a number from the string
       var course = courseTitle.match(/\d/g).join("");
       //Then remove the numbers 86 from the string
       course = course.split("86").join("");
-      console.log(course)
+      console.log(course);
       if (course.length == 1){
-         console.log("Made it into the conditional")
-         $('#courseSelect').append('<input onchange="changeInput()" type="text" id="specialTopics" class="form-control specialTopics" placeholder="enter special topics name" value=""/>')
+         console.log('inside');
+         $('#specialTopicsName').removeAttr('disabled');
+      }
+      else{
+         $('#specialTopicsName').attr('disabled','disabled');
       }
    }
 }
 
-function changeInput(){
-   $('#specialTopicsName').remove();
-   var text = document.getElementById('specialTopics').value;
-   console.log(text);
-   $('#addCourseForm').append('<input type="hidden" id="specialTopicsName" name="specialTopicName" value="'+text+'" />')
-   console.log("changeInput happened")
-}
 
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
