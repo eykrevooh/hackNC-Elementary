45faed2ded840cd941788aad4c066ed69188978b
Cody Myers <codymyers63@gmail.com>
2016-10-20 19:49:16 +0000

    Added some variables to make this script more dynamic

--- bannercourses2coursescomp.py
+++ bannercourses2coursescomp.py
@@ -8,35 +8,51 @@ try:
 except:
   print "tablee probably already exists"
 # csv has all the courses
-with open('Book2.csv', 'rb') as csvfile:
+problems = []
+with open('Book3.csv', 'rb') as csvfile:
   courses = csv.reader(csvfile)
   
   # disregard first entry its a header
   next(courses)
   for course in courses:
-    print course[1], course[2], course[3], course[9], course[8]
-    #number = int(re.search(r'\d+', course[2]).group())
-    number = course[2]
-    bannerRef = (BannerCourses.select().where(BannerCourses.subject == course[1])
+    # Find and put their column location in the csv here
+    ## Course Info ##
+    TID         = course[0]
+    subject     = course[1]
+    number      = course[2]
+    CRN         = course[9]
+    location    = None
+    title       = course[4]
+    ## Course Instructor Info ##
+    firstName   = course[7]
+    lastName    = course[8]
+    print CRN, subject, number, firstName, lastName 
+    
+    bannerRef = (BannerCourses.select().where(BannerCourses.subject == subject)
                                        .where(BannerCourses.number == number))
     if bannerRef.exists():
       print 'found banner ref'
     else:
-      number = int(re.search(r'\d+', course[2]).group())
-      bannerRef = (BannerCourses.select().where(BannerCourses.subject == course[1])
-                                       .where(BannerCourses.number == number))
-      
-       # these courses only have one instructor this may change 
-    # in the future
-    instructor = (User.select().where(User.firstName == course[9])
-                              .where(User.lastName == course[8]))
-
-    if instructor.exists():
-      instructorUsername = instructor[0].username
-    else:
-      instructorUsername = None
-    courseInBanner = CoursesInBanner(bannerRef = bannerRef[0].reFID,
-                                     instructor = instructorUsername)
-    courseInBanner.save()
+      bannerRef = None
+      issue = (str(CRN),str(subject),str(number),str(title))
+      problems.append(issue)
+    if bannerRef != None:
+      instructor = (User.select().where(User.firstName == firstName)
+                              .where(User.lastName == lastName))
+      if instructor.exists():
+        instructorUsername = instructor[0].username
+      else:
+        instructorUsername = None
+      courseInBanner = CoursesInBanner(bannerRef = bannerRef[0].reFID,
+                                        instructor = instructorUsername)
+      courseInBanner.save()
+  try:
+    f = open('Problems.csv','wt')
+    writer = csv.writer(f)
+    writer.writerow(('CRN','subject','number','title'))
+    for issue in problems:
+        writer.writerow(issue)
+  except Exception as e:
+    print e
 
                                      
