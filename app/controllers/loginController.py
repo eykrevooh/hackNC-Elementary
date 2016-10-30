23ed0333e7ec97f610c8eef7a030185bb2469af6
Ishwar Agarwal <a.isshwar@gmail.com>
2016-10-30 02:23:29 -0400

    merged all changes

--- app/controllers/loginController.py
+++ app/controllers/loginController.py
@@ -1,12 +1,6 @@
 # Login Controller
 
-from app import app
-from app.models import *
-
-from flask import \
-    render_template, \
-    redirect, \
-    request
+from app.allImports import *
 
 @app.route('/', methods=["GET","POST"])
 def homeReroute():
