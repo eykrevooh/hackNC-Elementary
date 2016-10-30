fbfcedf8a515d3b5ec68de1137e55db2cb3344c2
eykrevooh <hooverk@berea.edu>
2016-10-30 04:20:01 -0400

    updated post controller

--- app/controllers/postController.py
+++ app/controllers/postController.py
@@ -4,7 +4,21 @@ from app.allImports import *
 
 @app.route('/post/', methods=["GET","POST"])
 def postQuestion():
-    if request.method == 'POST':
-        #TODO: set values posted by form for question table
-        pass
-    return render_template('postView.html')
+    if request.method == "POST":
+        title = request.form["title"]
+        quest = request.form["question"]
+        course = request.form["courses"]
+        cId = Course.select().where(Course.course_num == course).get()
+        Question(
+             sID = 1,
+             cID = cId,
+             status = "PENDING",
+             assignment = "A10",
+             title = title,
+             question = quest
+        ).save()
+        return render_template('index.html')
+    else:
+        courses = Course.select()
+        return render_template('postView.html',
+                               courses = courses)
