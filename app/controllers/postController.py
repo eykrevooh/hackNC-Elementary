3c068b2b9b3242c2c96b732264718ef5be5a8502
Jesson Soto Ventura <Jesson@sotoventura.com>
2016-10-30 06:13:49 -0400

    Fixed navbar issues

--- app/controllers/postController.py
+++ app/controllers/postController.py
@@ -3,7 +3,8 @@
 from app.allImports import *
 
 @app.route('/post/', methods=["GET","POST"])
-def postQuestion():
+@app.route('/post/<perm>', methods=["GET","POST"])
+def postQuestion(perm=None):
     if request.method == "POST":
         title = request.form["title"]
         quest = request.form["question"]
@@ -21,8 +22,8 @@ def postQuestion():
                     .where(Question.question == quest)\
                     .get()
 
-        return redirect(url_for('watsonGetQuestion', qID = question.qID))
+        return redirect(url_for('watsonGetQuestion',param = perm, qID = question.qID))
     else:
         courses = Course.select()
         return render_template('postView.html',
-                               courses = courses)
+                               courses = courses, user=perm)
--- app/controllers/questionsController.py
+++ app/controllers/questionsController.py
@@ -2,15 +2,16 @@
 
 from app.allImports import *
 
-@app.route('/questions/', methods=["GET"])
-def questions():
+@app.route('/questions/<perm>', methods=["GET"])
+def questions(perm):
     questions = Question.select()
     print questions
     classes = Course.select()
     #two queries 2) all questions NOT associated with 'signed in user' and 1) all questions ONLY associated with 'signed in user'
     return render_template('questionsView.html',
                            questions = questions,
-                           classes = classes)
+                           classes = classes,
+                           user = perm)
 
 @app.route('/claim/', methods=["GET"])
 def claim():
